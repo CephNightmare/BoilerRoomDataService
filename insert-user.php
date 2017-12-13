ed9051133119300c5998a86254f8ab5e0b6cfe21
CephNightmare <geert_van_drunen@live.nl>
2017-12-13 17:28:34 +0100

    check username (test on DB)

--- insert-user.php
+++ insert-user.php
@@ -3,21 +3,21 @@
 header('Access-Control-Allow-Origin: *');
 
 // Define database connection parameters
-$hn      = 'localhost';
-$un      = 'boilerroomapp';
-$pwd     = 'boilerroomapp';
-$db      = 'boilerroomapp';
-$cs      = 'utf8';
+$hn = 'localhost';
+$un = 'boilerroomapp';
+$pwd = 'boilerroomapp';
+$db = 'boilerroomapp';
+$cs = 'utf8';
 
 // Set up the PDO parameters
-$dsn  = "mysql:host=" . $hn . ";port=3306;dbname=" . $db . ";charset=" . $cs;
-$opt  = array(
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+$dsn = "mysql:host=" . $hn . ";port=3306;dbname=" . $db . ";charset=" . $cs;
+$opt = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 );
 // Create a PDO instance (connect to the database)
-$pdo  = new PDO($dsn, $un, $pwd, $opt);
+$pdo = new PDO($dsn, $un, $pwd, $opt);
 
 // Attempt to run PDO prepared statement
 try {
@@ -27,34 +27,36 @@ try {
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $timeTarget = 0.03; // 30 milliseconds
-        $cost = 8;
-        do {
-            $cost++;
-            $start = microtime(true);
-            password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
-            $end = microtime(true);
-        } while (($end - $start) < $timeTarget);
+        $selectUserStmt = $pdo->query('SELECT Username FROM users WHERE Username == ' . $username . 'LIMIT 1');
+        $selectUserStmt->execute();
+        $userRow = $stmt->fetch();
 
-        $options = [
-            'cost' => $cost
-        ];
+        if (!empty($userRow)) {
+            $timeTarget = 0.03; // 30 milliseconds
+            $cost = 8;
+            do {
+                $cost++;
+                $start = microtime(true);
+                password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
+                $end = microtime(true);
+            } while (($end - $start) < $timeTarget);
 
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT, $options);
+            $options = [
+                'cost' => $cost
+            ];
 
-        $sql = "INSERT INTO `users` (Username, Password, Email) VALUES ('$username', '$hashedPassword', '$email')";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT, $options);
 
-        echo json_encode(array('message' => ''));
+            $sql = "INSERT INTO `users` (Username, Password, Email) VALUES ('$username', '$hashedPassword', '$email')";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
 
+            echo json_encode(array('message' => ''));
+        }
     }
 
-    echo json_encode(array('message' => 'reached data'));
+    echo json_encode(array('message' => 0));
 
-}
-
-catch(PDOException $e)
-{
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
